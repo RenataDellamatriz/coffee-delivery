dea652eab873026badfa2b99296a1d70cecfbad9
Renata Dellamatriz <r.dellamatriz@hotmail.com>
2023-02-25 16:31:28 -0300

    feat: location dialog

--- src/components/LocationDialog/index.tsx
+++ src/components/LocationDialog/index.tsx
@@ -1,7 +1,7 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import * as Select from "@radix-ui/react-select";
 import axios from "axios";
-import { CaretDown, MapPin } from "phosphor-react";
+import { CaretDown, Check, MapPin } from "phosphor-react";
 import { useEffect, useState } from "react";
 import { fetchStateData } from "../../services/locationApi/api";
 
@@ -10,53 +10,64 @@ import {
   LocationOverlay,
   LocationTitle,
   LocationTrigger,
+  SelectContainer,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
 } from "./styles";
 
-export interface StateProps {
+export interface UfProps {
   sigla: string;
   id: number;
-  name: string;
+  nome: string;
 }
 
 export interface CityProps {
   id: number;
-  name: string;
+  nome: string;
 }
 
 export function LocationDialog() {
-  const [state, setState] = useState<StateProps[]>([]);
+  const [uf, setUf] = useState<UfProps[]>([]);
+  const [seletedUf, setSelectedUf] = useState<string>();
   const [city, setCity] = useState<CityProps[]>([]);
+  const [selectedCity, setSelectedCity] = useState<string>();
 
   useEffect(() => {
-    async function getStateData() {
-      const getState = await fetchStateData();
-      setState(getState);
+    async function getUfData() {
+      const getUf = await fetchStateData();
+      setUf(getUf);
     }
-    getStateData();
+    getUfData();
   }, []);
 
-  // useEffect(() => {
-  //   async function getCityData() {
-  //     async function fetchCityData() {
-  //       try {
-  //         const { data } = await axios(
-  //           `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${state}/municipios`
-  //         );
-  //         return data;
-  //       } catch (error) {
-  //         console.log(error);
-  //       }
-  //     }
-  //     const getCity = await fetchCityData();
-  //     setCity(getCity);
-  //   }
-  //   getCityData();
-  // }, []);
+  useEffect(() => {
+    async function getCityData() {
+      try {
+        const { data } = await axios(
+          `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${seletedUf}/municipios`
+        );
 
+        setCity(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    if (seletedUf) {
+      getCityData();
+    }
+  }, [seletedUf]);
 
-  
+  function handleUfChange(uf: string) {
+    setSelectedUf(uf);
+    setSelectedCity(undefined);
+  }
 
-  const orderedStatesByName = state.sort(function (a, b) {
+  function handleCityChange(city: string) {
+    setSelectedCity(city);
+  }
+
+  const orderedStatesByName = uf.sort(function (a, b) {
     if (a.sigla < b.sigla) {
       return -1;
     }
@@ -65,70 +76,84 @@ export function LocationDialog() {
     }
     return 0;
   });
-  console.log(orderedStatesByName);
-
-  // const orderedCitiesByName =
 
   return (
     <Dialog.Root>
       <LocationTrigger>
         <MapPin weight="fill" />
-        Porto Alegre, RS
+        {selectedCity && seletedUf
+          ? `${selectedCity}, ${seletedUf}`
+          : "Escolha sua localização"}
       </LocationTrigger>
       <Dialog.Portal>
         <LocationOverlay />
         <LocationContent>
           <LocationTitle>Selecione sua localização</LocationTitle>
 
-          <Select.Root onValueChange={() =>setState(value)} required>
-            <Select.Trigger>
-              <Select.Value placeholder="Selecione um Estado" />
-              <Select.Icon>
-                <CaretDown />
-              </Select.Icon>
-            </Select.Trigger>
+          <SelectContainer>
+            <Select.Root
+              required
+              value={seletedUf}
+              onValueChange={handleUfChange}
+            >
+              <SelectTrigger>
+                <Select.Value placeholder="UF" />
+                <Select.Icon>
+                  <CaretDown />
+                </Select.Icon>
+              </SelectTrigger>
 
-            <Select.Portal>
-              <Select.Content>
-                <Select.Viewport>
-                  {orderedStatesByName.map((state) => {
-                    return (
-                      <Select.Item value={state.sigla}>
-                        <Select.ItemText key={state.id}>
-                          {state.sigla}
-                        </Select.ItemText>
-                      </Select.Item>
-                    );
-                  })}
-                </Select.Viewport>
-              </Select.Content>
-            </Select.Portal>
-          </Select.Root>
+              <Select.Portal>
+                <SelectContent>
+                  <Select.Viewport>
+                    <Select.Group>
+                      {orderedStatesByName.map((state) => {
+                        return (
+                          <SelectItem key={state.id} value={state.sigla}>
+                            <Select.ItemText>{state.sigla}</Select.ItemText>
+                            <Select.ItemIndicator>
+                              <Check />
+                            </Select.ItemIndicator>
+                          </SelectItem>
+                        );
+                      })}
+                    </Select.Group>
+                  </Select.Viewport>
+                </SelectContent>
+              </Select.Portal>
+            </Select.Root>
 
-          <Select.Root>
-            <Select.Trigger>
-              <Select.Value placeholder="Selecione um Estado" />
-              <Select.Icon>
-                <CaretDown />
-              </Select.Icon>
-            </Select.Trigger>
+            <Select.Root value={selectedCity} onValueChange={handleCityChange}>
+              <SelectTrigger>
+                <Select.Value placeholder="Cidade" />
+                <Select.Icon>
+                  <CaretDown />
+                </Select.Icon>
+              </SelectTrigger>
 
-            <Select.Portal>
-              <Select.Content>
-                <Select.Viewport>
-                  {orderedStatesByName.map((state) => {
-                    return (
-                      <Select.Item value={state.sigla}>
-                        <Select.ItemText key={state.id}>
-                          {state.sigla}
-                        </Select.ItemText>
-                      </Select.Item>
-                    );
-                  })}
-                </Select.Viewport>
-              </Select.Content>
-            </Select.Portal>
-          </Select.Root>
+              <Select.Portal>
+                <SelectContent>
+                  <Select.Viewport>
+                    {seletedUf &&
+                      city.map((state) => {
+                        return (
+                          <SelectItem
+                            disabled={!seletedUf}
+                            key={state.id}
+                            value={state.nome}
+                          >
+                            <Select.ItemText>{state.nome}</Select.ItemText>
+                            <Select.ItemIndicator>
+                              <Check />
+                            </Select.ItemIndicator>
+                          </SelectItem>
+                        );
+                      })}
+                  </Select.Viewport>
+                </SelectContent>
+              </Select.Portal>
+            </Select.Root>
+          </SelectContainer>
 
           <Dialog.Close />
         </LocationContent>
--- src/components/LocationDialog/styles.ts
+++ src/components/LocationDialog/styles.ts
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import * as Dialog from "@radix-ui/react-dialog";
+import * as Select from "@radix-ui/react-select";
 
 export const LocationTrigger = styled(Dialog.Trigger)`
   display: flex;
@@ -35,6 +36,11 @@ export const LocationOverlay = styled(Dialog.Overlay)`
 `;
 
 export const LocationContent = styled(Dialog.Content)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
   background-color: ${(props) => props.theme["base-card"]};
   border-radius: 6px;
   box-shadow: hsl(206 22% 7% / 35%) 0px 10px 38px -10px,
@@ -53,4 +59,54 @@ export const LocationTitle = styled(Dialog.Title)`
   font-family: "Baloo 2", cursive;
   text-align: center;
   font-size: 1.5rem;
+`;
+
+export const SelectContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  margin: 2rem 0 1rem 0;
+
+  gap: 1rem;
+  width: 200px;
+`;
+
+export const SelectTrigger = styled(Select.Trigger)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 6px;
+  position: relative;
+
+  font-size: 0.875rem;
+  font-family: "Roboto", sans-serif;
+  font-weight: 400;
+
+  color: ${(props) => props.theme["purple-dark"]};
+  background-color: ${(props) => props.theme["purple-light"]};
+
+  padding: 0.75rem;
+  border-radius: 6px;
+  border: none;
+
+  cursor: pointer;
+`;
+
+export const SelectContent = styled(Select.Content)`
+  color: ${(props) => props.theme["purple-dark"]};
+  background-color: ${(props) => props.theme["purple-light"]};
+
+  padding: 0.75rem;
+  border-radius: 6px;
+
+  font-size: 0.875rem;
+  font-family: "Roboto", sans-serif;
+  font-weight: 400;
+`;
+
+export const SelectItem = styled(Select.Item)`
+  line-height: 1;
+  padding: 0.75rem;
+  position: relative;
 `;
